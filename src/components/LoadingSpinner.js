48d4c413126a9a953d1ec0aa662bca7c11a9c40b
Alysia Lynn <itsalysialynn@Alysias-MacBook-Pro.local>
2020-10-25 16:12:28 -0700

    clean up

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,5 +1,5 @@
-import React from "react";
 import ClipLoader from "react-spinners/ClipLoader";
+import React from "react";
 import styled from "styled-components";
 import { colors } from "../theme";
 
