3e185ffb07632cc11032f59be98d767b9abe424a
Alysia Lynn <itsalysialynn@Alysias-MacBook-Pro.local>
2020-10-25 17:05:13 -0700

    style toast

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import Enzyme from "enzyme";
 import HomePage from "./components/HomePage";
 import React from "react";
-import { GlobalStyle } from "./theme";
+import styled from "styled-components";
+import { GlobalStyle, colors } from "./theme";
 import { ToastContainer } from "react-toastify";
 
 Enzyme.configure({ adapter: new Adapter() });
+
+const StyledToastContainer = styled(ToastContainer)`
+  .Toastify__toast--error {
+    background: ${colors.copper};
+    border-radius: 4px;
+    font-size: 16px;
+    padding: 16px;
+    font-family: "PT Sans", sans-serif;
+  }
+`;
 
 const App = () => (
   <>
     <GlobalStyle />
     <HomePage />
-    <ToastContainer
+    <StyledToastContainer
       position="bottom-right"
       autoClose={5000}
       closeOnClick
